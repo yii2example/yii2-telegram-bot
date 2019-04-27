ac3b163786287e86cc616055af1b74656f37460f
Vitaliy <theyamshikov@yandex.ru>
2019-04-27 21:34:56 +0600

    update data

--- common/data/package_provider.php
+++ common/data/package_provider.php
@@ -3,8 +3,15 @@
 use yii\helpers\ArrayHelper;
 
 $collection = [
-
+    [
+        'name' => 'yuwert',
+        'host' => 'http://git.yuwert.kz',
+        'url_templates' => [
+            'new_tag' => '{package}/tags/new',
+            'view_commit' => '{package}/commit/{hash}',
+        ],
+    ],
 ];
 
-$baseCollection = require_once(__DIR__ . '/../../vendor/yubundle/yii2-common/src/package/domain/data/package_provider.php');
+$baseCollection = require_once(__DIR__ . '/../../vendor/yii2rails/yii2-extension/src/package/domain/data/package_provider.php');
 return ArrayHelper::merge($baseCollection, $collection);
