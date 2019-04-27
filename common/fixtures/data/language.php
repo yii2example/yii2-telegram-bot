8cd26658345475f3b12353e19efdac54c8589a68
Vitaliy <theyamshikov@yandex.ru>
2019-04-27 22:53:29 +0600

    fix

--- common/fixtures/data/language.php
+++ common/fixtures/data/language.php
@@ -1,3 +1,20 @@
 <?php
 
-return include(__DIR__ . '/../../../vendor/yubundle/yii2-lang/src/domain/data/languages.php');
+return [
+    [
+        'title' => 'Русский',
+        'name' => 'Russian',
+        'code' => 'ru',
+        'locale' => 'ru-RU',
+        'is_main' => true,
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'English',
+        'name' => 'English',
+        'code' => 'en',
+        'locale' => 'en-UK',
+        'is_main' => false,
+        'is_enabled' => false,
+    ],
+];
