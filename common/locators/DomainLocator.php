024469733c40a7686e41d69dcc9e07aa5e6d1b33
Vitaliy <theyamshikov@yandex.ru>
2019-04-29 23:10:47 +0600

    style: define telegram domain

--- common/locators/DomainLocator.php
+++ common/locators/DomainLocator.php
@@ -3,6 +3,6 @@
 namespace common\locators;
 
 /**
- *
+ * @property-read \yii2bundle\telegram\domain\Domain $telegram
  */
 class DomainLocator extends \yubundle\common\project\common\locators\DomainLocator {}
